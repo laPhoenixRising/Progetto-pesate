43a0fd5b3ffe086221a0ba647048801d4ec6e781
Francesca Colombo <francesca.colo@gmail.com>
2024-03-07 23:19:07 +0100

    Persistito nel database l'inserimento della pesata

--- app/controllers/pesate_controller.rb
+++ app/controllers/pesate_controller.rb
@@ -2,9 +2,11 @@ class PesateController < ApplicationController
   include Pagy::Backend
 
   def archive
-   @pagy, @weighings = pagy(Weighing.all)
+   @pagy, @weighings = pagy(Weighing.all.order(created_at: :desc))
   end
   
   def create
+    Weighing.create(value: params[:weigh])
+    redirect_to "/"
   end
 end
