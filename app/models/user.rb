3589cbf5a43c33cde860028130849b9d5f6a4b32
Francesca Colombo <francesca.colo@gmail.com>
2024-03-15 14:52:54 +0100

    Validazione User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 8..16 }
+#la password deve avere almeno 8 caratteri misti
+
   has_many :weighings
 
   def archive
