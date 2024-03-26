132bfcf8bd0b6e4f65b456ce11f4641408c46f68
Francesca Colombo <francesca.colo@gmail.com>
2024-03-26 10:42:51 +0100

    Implementazione traduzioni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :setting_language
+  
   def must_be_logged
     redirect_to new_login_path if !session[:utente]
+  end
+
+  def setting_language
+    I18n.locale = :it
   end
 end
