082a45081ed055d9a3c77c1b8138efc47395b099
Francesca Colombo <francesca.colo@gmail.com>
2024-04-01 22:45:43 +0200

    Traduzione dei messaggi d'errore

--- app/models/weighing.rb
+++ app/models/weighing.rb
@@ -1,5 +1,5 @@
 class Weighing < ApplicationRecord
   belongs_to :user
-  validates :value, presence: true, comparison: { greater_than: 0 }
+  validates :value, comparison: { greater_than: 0 }
   validates :user_id, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Pesate
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :it
+    config.active_model.i18n_customize_full_message = true
   end
 end
