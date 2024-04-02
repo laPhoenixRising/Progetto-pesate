77988826ba282df582d8fcd033f5d1162c44a79d
Francesca Colombo <francesca.colo@gmail.com>
2024-04-02 22:17:42 +0200

    Test model User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "email should be present" do
+    user = User.create(password: "password")
+    assert(!user.save)
+  end
+
+  test "email format" do
+    user = User.create(email: "a.com", password: "password")
+    assert(!user.save)
+  end
+
+  test "email unique" do
+    User.create(email: "a@gmail.com", password: "password")
+    user = User.new(email: "a@gmail.com", password: "password")
+    assert(!user.save)
+  end
+
+  test "password should be present" do
+    user = User.create(email: "a@gmail.com")
+    assert(!user.save)
+  end
+
+  test "password at least 8 characters" do
+    user = User.create(email: "a@gmail.com", password: "pass")
+    assert(!user.save)
+  end
+
+  test "save" do
+    user = User.create(email: "a@gmail.com", password: "password")
+    assert(user.save)
+  end
+
 end
--- test/models/weighing_test.rb
+++ test/models/weighing_test.rb
@@ -4,30 +4,24 @@ class WeighingTest < ActiveSupport::TestCase
 
   test "weight without values doesn't save" do
     w = Weighing.new
-    result = w.save
-    assert(result == false)
+    assert(!w.save)
   end
 
   test "value > 0" do
     user = User.create(email: "a@gmail.com", password: "password")
     w = Weighing.new(value: -10, user: user)
-    result = w.save
-    assert(result == false)
+    assert(!w.save)
   end
 
   test "user should be present" do
     w = Weighing.new(value: 60)
-    result = w.save
-    assert(result == false) 
+    assert(!w.save) 
   end
 
   test "save" do
     user = User.create(email: "a@gmail.com", password: "password")
     w = Weighing.new(value: 50, user: user)
-    result = w.save
-    assert result
+    assert(w.save) 
   end
 
 end
-
-
