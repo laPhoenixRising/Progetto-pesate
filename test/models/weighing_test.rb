d61984b291e15919881e1ac0681968888509ede2
Francesca Colombo <francesca.colo@gmail.com>
2024-04-02 21:56:49 +0200

    Test model pesate

--- test/models/weighing_test.rb
+++ test/models/weighing_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class WeighingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "weight without values doesn't save" do
+    w = Weighing.new
+    result = w.save
+    assert(result == false)
+  end
+
+  test "value > 0" do
+    user = User.create(email: "a@gmail.com", password: "password")
+    w = Weighing.new(value: -10, user: user)
+    result = w.save
+    assert(result == false)
+  end
+
+  test "user should be present" do
+    w = Weighing.new(value: 60)
+    result = w.save
+    assert(result == false) 
+  end
+
+  test "save" do
+    user = User.create(email: "a@gmail.com", password: "password")
+    w = Weighing.new(value: 50, user: user)
+    result = w.save
+    assert result
+  end
+
 end
+
+
