482d359e784fef94ebf001299eece3ca4cc9dd24
Francesca Colombo <francesca.colo@gmail.com>
2024-04-04 23:32:50 +0200

    Rinomina del metodo signup confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,10 @@ class UserMailer < ApplicationMailer
 
   default from: 'notifications@example.com'
 
-  def welcome_email
+  def signup_confirmation
     @user = params[:user]
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
-  
+
 end
