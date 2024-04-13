042ad14ffabec163d72044cf1586e7f22472b889
Francesca Colombo <francesca.colo@gmail.com>
2024-04-13 19:52:29 +0200

    Cancellazione pesate prima di eliminazione utente

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,8 @@ class UserController < ApplicationController
 
   def destroy
     user = User.find(params[:id])
+    u = user.weighings
+    u.destroy
     user.destroy
 
     redirect_to signup_path
